1a07a237e3ca00e1277e6f8c7c404dd991df930e
naomysilva <naomyleticia92@gmail.com>
2023-11-06 13:59:52 -0300

    projeto finalizado

---
+++ src/components/Banner.vue
@@ -0,0 +1,29 @@
+<template >
+    <div id="main-banner">
+        <h1>Make Your Burger</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: "Banner"
+}
+</script>
+
+<style scoped>
+    #main-banner {
+        background-image: url(../assets/burger.jpg);
+        background-position: 0px 63%;
+        background-size: cover;
+        height: 543px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+    #main-banner h1 {
+        color: #fff;
+        text-align: center;
+        font-size: 60px;
+        background-color: #222;
+        padding: 20px 40px;
+    }
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer id="footer">
+        <p>Make Your Burguer &copy; 2021 </p>
+    </footer >
+</template>
+<script>
+export default {
+    name: 'Footer'
+    }
+</script>
+<style scoped>
+ #footer {
+    position: absolute;
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+ }
+</style>
